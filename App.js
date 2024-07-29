6a9fd3e87bf40b35b7730f35b34cd7d337c1063f
fyfly9090 <fengyun0706@gmail.com>
2024-07-29 15:35:22 -0400

    installed mongodb

--- App.js
+++ App.js
@@ -1,18 +1,13 @@
 import "dotenv/config";
 import session from "express-session";
 import express from "express";
-import Hello from "./Hello.js";
-import Lab5 from "./Lab5/index.js";
-import QueryParameters from "./Lab5/QueryParameters.js";
-import WorkingWithObjects from "./Lab5/WorkingWithObjects.js";
-import MakeModule from "./Lab5/MakeModule.js";
-import WorkingWithArrays from "./Lab5/WorkingWithArrays.js";
 import cors from "cors";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import mongoose from "mongoose";
 import UserRoutes from "./Users/routes.js";
+import Hello from "./Hello.js";
 
 const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
 mongoose.connect(CONNECTION_STRING);
@@ -44,12 +39,8 @@ ModuleRoutes(app);
 CourseRoutes(app);
 AssignmentRoutes(app);
 UserRoutes(app);
-
 Hello(app);
-Lab5(app);
-QueryParameters(app);
-WorkingWithObjects(app);
-MakeModule(app);
-WorkingWithArrays(app);
+
+
 
 app.listen( process.env.PORT || 4000)
